d9aa8168802188011aa7b795b1b493e962ed6a32
Sai prashanth <prashanthshivakoti@gmail.com>
2025-04-21 18:40:36 +0530

    Add files via upload
    A simple and elegant Todo List app built using React and Bootstrap. This project lets users add, mark complete/incomplete, and delete tasks, with localStorage support to retain tasks across browser sessions.

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Todolist from './Todolist';
+
+function App(){
+  return (
+    <>
+<Todolist/>
+    </>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
